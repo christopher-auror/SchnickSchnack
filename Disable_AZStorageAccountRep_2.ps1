44d386e68c465b9920438f5ac00f8599a54b1f2c
Christopher Hipwell <christopher.hipwell@auror.co>
2023-07-31 08:22:09 +1200

    deleted file

--- Disable_AZStorageAccountRep_2.ps1
+++
@@ -1,7 +0,0 @@
-# Update a Storage account with AllowCrossTenantReplication
-
-$account = Set-AzStorageAccount -ResourceGroupName "devChristopher" -Name "testchristopher" -AllowCrossTenantReplication $false -EnableHttpsTrafficOnly $true
-
-$account.AllowCrossTenantReplication
-
-False
