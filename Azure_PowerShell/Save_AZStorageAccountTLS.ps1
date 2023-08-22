3db8246a754cab8dd7a6f3310b2147bb987c66f1
Christopher Hipwell <christopher.hipwell@auror.co>
2023-08-22 15:40:50 +1200

    changed variable name

--- Azure_PowerShell/Save_AZStorageAccountTLS.ps1
+++ Azure_PowerShell/Save_AZStorageAccountTLS.ps1
@@ -6,8 +6,8 @@ $minimumTlsVersion = "TLS1_2"
 $storageAccounts = Get-AzStorageAccount -ResourceGroupName $rgName
 
 # Loop through each storage account and set the minimum TLS version
-foreach ($storageAccount in $storageAccounts) {
+foreach ($account in $storageAccounts) {
 	Set-AzStorageAccount -ResourceGroupName $rgName `
-	-Name $storageAccount.StorageAccountName `
+	-Name $account.StorageAccountName `
 	-MinimumTlsVersion $minimumTlsVersion
 }
