3d985e541bb7494a6f69a10135043ec5534c9685
Christopher Hipwell <christopher.hipwell@auror.co>
2023-10-17 10:38:22 +1300

    added new folder and moved files

---
+++ Miscellaneous/Azure_SQL_check_active_sessions.sql
@@ -0,0 +1,33 @@
+
+-- count of active SQL sessions
+SELECT COUNT(session_id) AS [Active Connections] FROM sys.dm_exec_sessions
+
+-- login time of sessions
+SELECT * FROM sys.dm_exec_sessions
+
+-- get more details from active connections
+SELECT 
+    session_id, 
+    login_name, 
+    host_name, 
+    status, 
+    client_interface_name
+FROM 
+    sys.dm_exec_sessions
+WHERE 
+    is_user_process = 1;
+
+-- see current requests
+SELECT 
+    session_id,
+    start_time,
+    status,
+    command,
+    sql_handle,
+    statement_start_offset,
+    statement_end_offset
+    database_id
+FROM 
+    sys.dm_exec_requests;
+
+
