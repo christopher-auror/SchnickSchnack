624d0c09d43eb9887529ed7413e95f942b755613
Christopher Hipwell <christopher.hipwell@auror.co>
2023-11-09 15:51:25 +1300

    Add try-catch block to handle errors

--- Microsoft_Graph_PowerShell/AZ_MEID_Show_All_AppReg_Secrets.ps1
+++ Microsoft_Graph_PowerShell/AZ_MEID_Show_All_AppReg_Secrets.ps1
@@ -4,34 +4,39 @@ Connect-AzAccount
 #secret expiration date filter (for example 30 days)
 $LimitExpirationDays = 30
 
-#Retrieving the list of secrets that expires in the above range of days
-$SecretsToExpire = Get-MgApplication -All $true | ForEach-Object {
-    $app = $_
-    @(
-        Get-MgPasswordCredential -ApplicationId $_.Id
-        Get-MgKeyCredential -ApplicationId $_.Id
-    ) | Where-Object {
-        $_.EndDate -lt (Get-Date).AddDays($LimitExpirationDays)
-    } | ForEach-Object {
-        $id = "Not set"
-        if($_.CustomKeyIdentifier) {
-            $id = [System.Text.Encoding]::UTF8.GetString($_.CustomKeyIdentifier)
-        }
-        [PSCustomObject] @{
-            App = $app.DisplayName
-            ObjectID = $app.Id
-            AppId = $app.AppId
-            Type = $_.GetType().name
-            KeyIdentifier = $id
-            EndDate = $_.EndDate
+try {
+    #Retrieving the list of secrets that expires in the above range of days
+    $SecretsToExpire = Get-MgApplication -All $true | ForEach-Object {
+        $app = $_
+        @(
+            Get-MgPasswordCredential -ApplicationId $_.Id
+            Get-MgKeyCredential -ApplicationId $_.Id
+        ) | Where-Object {
+            $_.EndDate -lt (Get-Date).AddDays($LimitExpirationDays)
+        } | ForEach-Object {
+            $id = "Not set"
+            if($_.CustomKeyIdentifier) {
+                $id = [System.Text.Encoding]::UTF8.GetString($_.CustomKeyIdentifier)
+            }
+            [PSCustomObject] @{
+                App = $app.DisplayName
+                ObjectID = $app.Id
+                AppId = $app.AppId
+                Type = $_.GetType().name
+                KeyIdentifier = $id
+                EndDate = $_.EndDate
+            }
         }
     }
-}
 
-#Printing the list of secrets that are near to expire
-if($SecretsToExpire.Count -EQ 0) {
-    Write-Output "No secrets found that will expire in this range"
+    #Printing the list of secrets that are near to expire
+    if($SecretsToExpire.Count -EQ 0) {
+        Write-Output "No secrets found that will expire in this range"
+    }
+    else {
+        Write-Output $SecretsToExpire
+    }
 }
-else {
-    Write-Output $SecretsToExpire
+catch {
+    Write-Error "An error occurred: $_"
 }
