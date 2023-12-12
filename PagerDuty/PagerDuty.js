3a55465c876c61eb94ed4830ebccdd8150d73a69
Christopher Hipwell <christopher.hipwell@auror.co>
2023-12-13 12:29:10 +1300

    Commented out unused code in PagerDuty.js

--- PagerDuty/PagerDuty.js
+++ PagerDuty/PagerDuty.js
@@ -1,7 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as pagerduty from "@pulumi/pagerduty";
 
-const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
+// not in use currently
+// const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
 const foo = new pagerduty.EscalationPolicy("foo", {
     numLoops: 2,
     rules: [{
