62fb2e57c1f1570b7db04508f57617df08c8a9ac
Christopher Hipwell <christopher.hipwell@auror.co>
2023-12-13 12:25:53 +1300

    Add exampleUser and exampleService to PagerDuty.js

--- PagerDuty/PagerDuty.js
+++ PagerDuty/PagerDuty.js
@@ -1,7 +1,24 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as pagerduty from "@pulumi/pagerduty";
 
-const demoTeam = new pagerduty.Team("demo-team", {
-    description: "Demo team generated from examples",
+const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
+const foo = new pagerduty.EscalationPolicy("foo", {
+    numLoops: 2,
+    rules: [{
+        escalationDelayInMinutes: 10,
+        targets: [{
+            type: "user_reference",
+            id: exampleUser.id,
+        }],
+    }],
 });
-
+const exampleService = new pagerduty.Service("exampleService", {
+    autoResolveTimeout: "14400",
+    acknowledgementTimeout: "600",
+    escalationPolicy: foo.id,
+    alertCreation: "create_alerts_and_incidents",
+    autoPauseNotificationsParameters: {
+        enabled: true,
+        timeout: 300,
+    },
+});
