0832fe37d79f402f515cf2cdfb04361bf05ba4d7
Christopher Hipwell <christopher.hipwell@auror.co>
2023-12-13 14:28:11 +1300

    Add PagerDuty user and escalation policy

--- PagerDuty/index.ts
+++ PagerDuty/index.ts
@@ -1,1 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as pagerduty from "@pulumi/pagerduty";
+
+const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
+// not in use currently
+// const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
+const foo = new pagerduty.EscalationPolicy("foo", {
+    numLoops: 2,
+    rules: [{
+        escalationDelayInMinutes: 10,
+        targets: [{
+            type: "user_reference",
+            id: exampleUser.id,
