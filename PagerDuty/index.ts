7ee30347bf94f92ad5f7b771da41af85e491639e
Christopher Hipwell <christopher.hipwell@auror.co>
2023-12-14 11:13:12 +1300

    Add new escalation policy, create service with auto-resolve and acknowledgement timeouts

--- PagerDuty/index.ts
+++ PagerDuty/index.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as pagerduty from "@pulumi/pagerduty";
 
-const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
-const foo = new pagerduty.EscalationPolicy("foo", {
+const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"}); // generates a new licensed user
+const foo = new pagerduty.EscalationPolicy("foo", { //generates a new escalation policy
     numLoops: 2,
     rules: [{
         escalationDelayInMinutes: 10,
@@ -12,3 +12,14 @@ const foo = new pagerduty.EscalationPolicy("foo", {
         }]
     }]
 });
+
+// Provide the Escalation Policy ID which is required for the service
+let service = new pagerduty.Service("my-service", {
+    escalationPolicy: "Escalation Policy ID here",
+    autoResolveTimeout: "14400", // Auto resolve incidents after 4 hours
+    acknowledgementTimeout: "1800", // Auto escalate incidents after 30 minutes
+    name: "My Service" // Human friendly name
+});
+
+// Confirm the ID of the service created for future reference if needed
+export const serviceId = service.id;
