b7ed141d0bab60fd113d864a42932ef4f133bbcf
Christopher Hipwell <christopher.hipwell@auror.co>
2023-12-13 15:52:22 +1300

    Remove unused code in PagerDuty/index.ts

--- PagerDuty/index.ts
+++ PagerDuty/index.ts
@@ -2,8 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as pagerduty from "@pulumi/pagerduty";
 
 const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
-// not in use currently
-// const exampleUser = new pagerduty.User("exampleUser", {email: "125.greenholt.earline@graham.name"});
 const foo = new pagerduty.EscalationPolicy("foo", {
     numLoops: 2,
     rules: [{
