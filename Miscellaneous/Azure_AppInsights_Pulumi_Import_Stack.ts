0d634a82da9bac50ff5e423b10b8b4265902c29b
Christopher Hipwell <christopher.hipwell@auror.co>
2024-03-12 11:22:20 +1300

    changed -s to --stack

--- Miscellaneous/Azure_AppInsights_Pulumi_Import_Stack.ts
+++ Miscellaneous/Azure_AppInsights_Pulumi_Import_Stack.ts
@@ -15,75 +15,75 @@ pulumi import azure-native:sql:Server SERVERSQL1 /subscriptions/aaaaaa......
 
 // Fawkes Prod AU
 
-/// fx-prodau-site-client-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-site-client-app-insight
+/// fx-prodau--stackite-client-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau--stackite-client-app-insight
 
-pulumi import azure-native:insights:Component fx-prodau-site-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-site-client-app-insight -s FawkesProdAu
+pulumi import azure-native:insights:Component fx-prodau--stackite-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau--stackite-client-app-insight --stack FawkesProdAu
 
 /// fx-prodau-web-app-insight
 /// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-web-app-insight
 
-pulumi import azure-native:insights:Component fx-prodau-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-web-app-insight -s FawkesProdAu
+pulumi import azure-native:insights:Component fx-prodau-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-web-app-insight --stack FawkesProdAu
 
 // Fawkes Prod NZ
 
-/// fx-prodnz-site-client-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdNz/providers/microsoft.insights/components/fx-prodnz-site-client-app-insight
+/// fx-prodnz--stackite-client-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdNz/providers/microsoft.insights/components/fx-prodnz--stackite-client-app-insight
 
-pulumi import azure-native:insights:Component fx-prodnz-site-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdNz/providers/microsoft.insights/components/fx-prodnz-site-client-app-insight -s FawkesProdNz
+pulumi import azure-native:insights:Component fx-prodnz--stackite-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdNz/providers/microsoft.insights/components/fx-prodnz--stackite-client-app-insight --stack FawkesProdNz
 
 /// fx-prodau-web-app-insight
 /// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-web-app-insight
 
-pulumi import azure-native:insights:Component fx-prodau-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-web-app-insight -s FawkesProdNz
+pulumi import azure-native:insights:Component fx-prodau-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdAu/providers/microsoft.insights/components/fx-prodau-web-app-insight --stack FawkesProdNz
 
 // Fawkes Prod UK
 
-/// fx-produk-site-client-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk-site-client-app-insight
+/// fx-produk--stackite-client-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk--stackite-client-app-insight
 
-pulumi import azure-native:insights:Component fx-produk-site-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk-site-client-app-insight -s FawkesProdUk
+pulumi import azure-native:insights:Component fx-produk--stackite-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk--stackite-client-app-insight --stack FawkesProdUk
 
 ///  fx-produk-web-app-insight
 ///  /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk-web-app-insight
 
-pulumi import azure-native:insights:Component fx-produk-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk-web-app-insight -s FawkesProdUk
+pulumi import azure-native:insights:Component fx-produk-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUk/providers/microsoft.insights/components/fx-produk-web-app-insight --stack FawkesProdUk
 
 //  Fawkes Prod US
 
-/// fx-produs-site-client-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs-site-client-app-insight
+/// fx-produs--stackite-client-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs--stackite-client-app-insight
 
-pulumi import azure-native:insights:Component fx-produs-site-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs-site-client-app-insight -s FawkesProdUs
+pulumi import azure-native:insights:Component fx-produs--stackite-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs--stackite-client-app-insight --stack FawkesProdUs
 
 /// fx-produs-web-app-insight
 /// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs-web-app-insight
 
-pulumi import azure-native:insights:Component fx-produs-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs-web-app-insight -s FawkesProdUs
+pulumi import azure-native:insights:Component fx-produs-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesProdUs/providers/microsoft.insights/components/fx-produs-web-app-insight --stack FawkesProdUs
 
 // FAWKES QA
 
-/// fx-qa-site-client-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa-site-client-app-insight
+/// fx-qa--stackite-client-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa--stackite-client-app-insight
 
-pulumi import azure-native:insights:Component fx-qa-site-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa-site-client-app-insight -s FawkesQa
+pulumi import azure-native:insights:Component fx-qa--stackite-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa--stackite-client-app-insight --stack FawkesQa
 
 /// fx-qa-web-app-insight
 /// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa-web-app-insight
 
-pulumi import azure-native:insights:Component fx-qa-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa-web-app-insight -s FawkesQa
+pulumi import azure-native:insights:Component fx-qa-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesQa/providers/microsoft.insights/components/fx-qa-web-app-insight --stack FawkesQa
 
 // FAWKES Sandbox
 
-/// fx-sb-site-client-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx-sb-site-client-app-insight
+/// fx--stackb--stackite-client-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx--stackb--stackite-client-app-insight
 
-pulumi import azure-native:insights:Component fx-sb-site-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx-sb-site-client-app-insight -s FawkesSandbox
+pulumi import azure-native:insights:Component fx--stackb--stackite-client-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx--stackb--stackite-client-app-insight --stack FawkesSandbox
 
-/// fx-sb-web-app-insight
-/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx-sb-web-app-insight
+/// fx--stackb-web-app-insight
+/// /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx--stackb-web-app-insight
 
-pulumi import azure-native:insights:Component fx-sb-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx-sb-web-app-insight -s FawkesSandbox
+pulumi import azure-native:insights:Component fx--stackb-web-app-insight /subscriptions/078fbcee-c3c5-464f-a52c-3a971648cdbc/resourceGroups/FawkesSandbox/providers/microsoft.insights/components/fx--stackb-web-app-insight --stack FawkesSandbox
 
 
 
