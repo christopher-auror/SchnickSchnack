d020589d5266b0c9c2cdf5357556774b86255f63
Christopher Hipwell <christopher.hipwell@auror.co>
2024-08-20 11:03:31 +1200

    Add Azure role assignment report script for Azure Automation Account

---
+++ Azure_Automation_Account/AZ_Role_Assignment_Report.ps1
@@ -0,0 +1,86 @@
+# Define the path of the CSV file to store the Role Assignment Report
+$csvPath = ".\Auror-Az-Role-Assignment-Report.csv"
+# Define the Log Analytics workspace ID
+$workspaceId = "b316f85a-19b4-42ab-bb06-9cfbef44826d"
+
+#Connect to managed identity in our Azure tenant
+# Connect-AzAccount -Identity
+
+#Log Analytics query for retrieving Role Assignment addition activities for the past 2 days
+    $addqr = 'AzureActivity
+    | where TimeGenerated > ago(2d)
+    | where CategoryValue =~ "Administrative" and  OperationNameValue =~ "Microsoft.Authorization/roleAssignments/write" and ActivityStatusValue =~ "Start"
+    | extend RoleDefinition = extractjson("$.Properties.RoleDefinitionId",tostring(Properties_d.requestbody),typeof(string))
+    | extend PrincipalId = extractjson("$.Properties.PrincipalId",tostring(Properties_d.requestbody),typeof(string))
+    | extend PrincipalType = extractjson("$.Properties.PrincipalType",tostring(Properties_d.requestbody),typeof(string))
+    | extend Scope = extractjson("$.Properties.Scope",tostring(Properties_d.requestbody),typeof(string))
+    | extend RoleId = split(RoleDefinition,"/")
+    | extend InitiatedBy = Caller
+    | extend Operation = split(OperationNameValue,"/")
+    | project TimeGenerated,InitiatedBy,Scope,PrincipalId,PrincipalType,RoleID=RoleId[4],Operation= Operation[2]'
+
+
+#Log Analytics query for retrieving Role Assignment removal activities for the past 2 days
+$rmqr = 'AzureActivity
+| where TimeGenerated > ago(2d)
+| where CategoryValue =~ "Administrative" and OperationNameValue =~ "Microsoft.Authorization/roleAssignments/delete" and (ActivityStatusValue =~ "Success")
+| extend RoleDefinition = extractjson("$.properties.roleDefinitionId",tostring(Properties_d.responseBody),typeof(string))
+| extend PrincipalId = extractjson("$.properties.principalId",tostring(Properties_d.responseBody),typeof(string))
+| extend PrincipalType = extractjson("$.properties.principalType",tostring(Properties_d.responseBody),typeof(string))
+| extend Scope = extractjson("$.properties.scope",tostring(Properties_d.responseBody),typeof(string))
+| extend RoleId = split(RoleDefinition,"/")
+| extend InitiatedBy = Caller
+| extend Operation = split(OperationNameValue,"/")
+| project TimeGenerated,InitiatedBy,Scope,PrincipalId,PrincipalType,RoleID=RoleId[6],Operation= Operation[2]'
+
+#Executing the Log Analytics queries
+$addqueryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $addqr
+$rmqueryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $rmqr
+
+#Isolating Log Analytics query results
+$addqrs = $addqueryResults.Results
+$rmqrs = $rmqueryResults.Results
+
+#For each add query result find user/group name and role name to append into the CSV report
+foreach ($qr in $addqrs)
+{
+$rd = Get-AzRoleDefinition -Id $qr.RoleID
+if($qr.PrincipalType -eq 'User')
+    {     
+        $prncpl = Get-AzADUser -ObjectId $qr.PrincipalId
+    }
+elseif($qr.PrincipalType -eq 'Group'){
+        $prncpl = Get-AzADGroup -ObjectId $qr.PrincipalId
+    }
+else{
+        $prncpl = Get-AzADServicePrincipal -ObjectId $qr.PrincipalId
+    }
+$qr | Add-Member -MemberType NoteProperty -Name 'Role' -Value $rd.Name
+$qr | Add-Member -MemberType NoteProperty -Name 'PrincipalName' -Value $prncpl.DisplayName
+
+#Exporting the results to a CSV file
+$qr | Export-Csv -Path $csvPath -NoTypeInformation -Append
+}
+
+#For each remove query result find user/group name and role name to append into the CSV report
+foreach ($qr in $rmqrs)
+{
+$rd = Get-AzRoleDefinition -Id $qr.RoleID
+if($qr.PrincipalType -eq 'User')
+    {     
+        $prncpl = Get-AzADUser -ObjectId $qr.PrincipalId
+    }
+elseif($qr.PrincipalType -eq 'Group'){
+        $prncpl = Get-AzADGroup -ObjectId $qr.PrincipalId
+    }
+else{
+        $prncpl = Get-AzADServicePrincipal -ObjectId $qr.PrincipalId
+    }
+$qr | Add-Member -MemberType NoteProperty -Name 'Role' -Value $rd.Name
+$qr | Add-Member -MemberType NoteProperty -Name 'PrincipalName' -Value $prncpl.DisplayName
+
+#Exporting the results to a CSV file
+$qr | Export-Csv -Path $csvPath -NoTypeInformation -Append
+}
+
+# End of Script
