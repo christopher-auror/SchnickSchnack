e0b34ab2076f11e01bb961e383fe921892691f4a
Christopher Hipwell <christopher.hipwell@auror.co>
2024-08-20 11:55:42 +1200

    improved error handling and parameter validation

--- Azure_Automation_Account/AZ_Role_Assignment_Report.ps1
+++ Azure_Automation_Account/AZ_Role_Assignment_Report.ps1
@@ -12,13 +12,14 @@ param(
     $workspaceId = "b316f85a-19b4-42ab-bb06-9cfbef44826d"
 )
 
-#Connect to managed identity in our Azure tenant
-Connect-AzAccount -Identity -ErrorAction Stop
+try {
+    # Connect to managed identity in our Azure tenant
+    Connect-AzAccount -Identity -ErrorAction Stop
 
-#Set the active subscription
-Set-AzContext -SubscriptionId $SubscriptionId -ErrorAction Stop
+    # Set the active subscription
+    Set-AzContext -SubscriptionId $SubscriptionId -ErrorAction Stop
 
-#Log Analytics query for retrieving Role Assignment addition activities for the past 2 days
+    # Log Analytics query for retrieving Role Assignment addition activities for the past 2 days
     $addqr = 'AzureActivity
     | where TimeGenerated > ago(2d)
     | where CategoryValue =~ "Administrative" and  OperationNameValue =~ "Microsoft.Authorization/roleAssignments/write" and ActivityStatusValue =~ "Start"
@@ -31,23 +32,33 @@ Set-AzContext -SubscriptionId $SubscriptionId -ErrorAction Stop
     | extend Operation = split(OperationNameValue,"/")
     | project TimeGenerated,InitiatedBy,Scope,PrincipalId,PrincipalType,RoleID=RoleId[4],Operation= Operation[2]'
 
+    # Log Analytics query for retrieving Role Assignment removal activities for the past 2 days
+    $rmqr = 'AzureActivity
+    | where TimeGenerated > ago(2d)
+    | where CategoryValue =~ "Administrative" and OperationNameValue =~ "Microsoft.Authorization/roleAssignments/delete" and (ActivityStatusValue =~ "Success")
+    | extend RoleDefinition = extractjson("$.properties.roleDefinitionId",tostring(Properties_d.responseBody),typeof(string))
+    | extend PrincipalId = extractjson("$.properties.principalId",tostring(Properties_d.responseBody),typeof(string))
+    | extend PrincipalType = extractjson("$.properties.principalType",tostring(Properties_d.responseBody),typeof(string))
+    | extend Scope = extractjson("$.properties.scope",tostring(Properties_d.responseBody),typeof(string))
+    | extend RoleId = split(RoleDefinition,"/")
+    | extend InitiatedBy = Caller
+    | extend Operation = split(OperationNameValue,"/")
+    | project TimeGenerated,InitiatedBy,Scope,PrincipalId,PrincipalType,RoleID=RoleId[6],Operation= Operation[2]'
+}
+catch {
+    Write-Error "An error occurred: $_"
+    exit 1
+}
 
-#Log Analytics query for retrieving Role Assignment removal activities for the past 2 days
-$rmqr = 'AzureActivity
-| where TimeGenerated > ago(2d)
-| where CategoryValue =~ "Administrative" and OperationNameValue =~ "Microsoft.Authorization/roleAssignments/delete" and (ActivityStatusValue =~ "Success")
-| extend RoleDefinition = extractjson("$.properties.roleDefinitionId",tostring(Properties_d.responseBody),typeof(string))
-| extend PrincipalId = extractjson("$.properties.principalId",tostring(Properties_d.responseBody),typeof(string))
-| extend PrincipalType = extractjson("$.properties.principalType",tostring(Properties_d.responseBody),typeof(string))
-| extend Scope = extractjson("$.properties.scope",tostring(Properties_d.responseBody),typeof(string))
-| extend RoleId = split(RoleDefinition,"/")
-| extend InitiatedBy = Caller
-| extend Operation = split(OperationNameValue,"/")
-| project TimeGenerated,InitiatedBy,Scope,PrincipalId,PrincipalType,RoleID=RoleId[6],Operation= Operation[2]'
-
-#Executing the Log Analytics queries
-$addqueryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $addqr
-$rmqueryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $rmqr
+try {
+    # Executing the Log Analytics queries
+    $addqueryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $addqr -ErrorAction Stop
+    $rmqueryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $rmqr -ErrorAction Stop
+}
+catch {
+    Write-Error "An error occurred while executing the queries: $_"
+    exit 1
+}
 
 #Isolating Log Analytics query results
 $addqrs = $addqueryResults.Results
