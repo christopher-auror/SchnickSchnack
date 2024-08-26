495e3335f0f9d762d9003d730ae038ce5c99453f
Christopher Hipwell <christopher.hipwell@auror.co>
2024-08-27 11:06:16 +1200

    basic version

---
+++ Azure_PowerShell/AZ_Disk_Remove.ps1
@@ -0,0 +1,17 @@
+# Authenticate to Azure
+# Connect-AzAccount
+
+# Define the query to find disks that haven't had ownership updates in the last 90 days
+$disksToBeRemoved = Search-AzGraph -Query '
+resources
+| where type == "microsoft.compute/disks"
+| where todatetime(properties.LastOwnershipUpdateTime) < ago(90d)
+| project name, diskState = properties.diskState, lastUpdateTime = format_datetime(todatetime(properties.LastOwnershipUpdateTime), "dd-MM-yyyy")
+'
+
+# Loop through each disk and delete it
+foreach ($disk in $disksToBeRemoved) {
+    # Output the disk information for verification
+    Write-Output "Disk: $($disk.name), Last Update: $($disk.lastUpdateTime)"
+    
+}
