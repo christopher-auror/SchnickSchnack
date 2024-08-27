4ceaa83df387f9da0a9be0612c426379872b0114
Christopher Hipwell <christopher.hipwell@auror.co>
2024-08-27 13:11:25 +1200

    Add script to remove unattached disks older than 90 days

---
+++ Azure_Automation_Account/AZ_Disk_Remove.ps1
@@ -0,0 +1,18 @@
+# Authenticate to Azure
+Connect-AzAccount
+
+# Define the query to find disks that haven't had ownership updates in the last 90 days
+$disksToBeRemoved = Search-AzGraph -Query '
+resources
+| where type == "microsoft.compute/disks"
+| where todatetime(properties.LastOwnershipUpdateTime) < ago(90d)
+| where properties.diskState == "Unattached"
+| project name, diskState = properties.diskState, lastUpdateTime = format_datetime(todatetime(properties.LastOwnershipUpdateTime), "dd-MM-yyyy")
+'
+
+# Loop through each disk and output the disk information
+foreach ($disk in $disksToBeRemoved) {
+    # Output the disk information for verification
+    Write-Output "Disk: $($disk.name), Last Update: $($disk.lastUpdateTime)"
+    
+}
