944bf7acb9db9fd00885b332bfb0e3925ee5a05e
Christopher Hipwell <christopher.hipwell@auror.co>
2024-10-22 13:11:52 +1300

    fix feature flag use

--- CHIPwell_Pulumi/index.ts
+++ CHIPwell_Pulumi/index.ts
@@ -7,19 +7,26 @@ import "./vm"; // Import the VM module
 // Create an Azure Resource Group
 const resourceGroup = 'devChristopher';
 
-// Create an Azure Storage Account
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
-});
+const config = new pulumi.Config();
+const enableSA = config.getBoolean("enableSA") ?? true;
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = storage.listStorageAccountKeysOutput({
-    resourceGroupName: resourceGroup,
-    accountName: storageAccount.name
-});
+let sa;
+let storageAccountKeys;
+if (enableSA) {
+    // Create an Azure Storage Account
+    const storageAccount = new storage.StorageAccount("sa", {
+        resourceGroupName: resourceGroup,
+        sku: {
+            name: storage.SkuName.Standard_LRS,
+        },
+        kind: storage.Kind.StorageV2,
+    });
 
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+    // Export the primary key of the Storage Account
+    storageAccountKeys = storage.listStorageAccountKeysOutput({
+        resourceGroupName: resourceGroup,
+        accountName: storageAccount.name
+    });
+}
+
+export const primaryStorageKey = enableSA && storageAccountKeys ? storageAccountKeys.keys[0].value : undefined;
