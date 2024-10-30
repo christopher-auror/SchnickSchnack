e5ec249f0a73952e1ca119fc1c4c4cc50e855e4e
Christopher Hipwell <christopher.hipwell@auror.co>
2024-10-30 17:10:29 +1300

    update RecoveryVault to enable vault creation by default and refactor backup policy configuration

--- CHIPwell_Pulumi/RecoveryVault.ts
+++ CHIPwell_Pulumi/RecoveryVault.ts
@@ -1,18 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure_native from "@pulumi/azure-native";
+import * as azure_native from "@pulumi/azure-native"
+import * as recoveryservices from "@pulumi/azure-native/recoveryservices/v20240430preview";
 
 // Create or read an Azure Resource Group
 const resourceGroup = 'devChristopher';
 
 const config = new pulumi.Config();
-const enableVault = config.getBoolean("enableVault") ?? false;
+const enableVault = config.getBoolean("enableVault") ?? true;
 
 let vault;
+// Create a Recovery Services Vault
 if (enableVault) {
-    // Create a Recovery Services Vault
-    vault = new azure_native.recoveryservices.Vault("vault", {
+    vault = new recoveryservices.Vault("vault", {
         identity: {
-            type: azure_native.recoveryservices.ResourceIdentityType.SystemAssigned,
+            type: recoveryservices.ResourceIdentityType.SystemAssigned,
         },
         location: "Australia East",
         properties: {
@@ -28,49 +29,36 @@ if (enableVault) {
         },
         resourceGroupName: resourceGroup,
         sku: {
-            name: azure_native.recoveryservices.SkuName.Standard,
+            name: recoveryservices.SkuName.Standard,
         },
         vaultName: "CHIPWellVault",
     });
 }
 
-/* // Create a Backup Policy
-const backupPolicy = new azure_native.dataprotection.BackupPolicy("exampleBackupPolicy", {
-    resourceGroupName: "DevChristopher",
-    vaultName: vault.name,
-    backupPolicyName: "exampleBackupPolicy",
-    properties: {
-        policyRules: [
-            {
-                name: "DailyBackup",
-                objectType: "AzureBackupRule",
-                backupParameters: {
-                    backupType: "Full",
-                    objectType: "AzureBackupParams",
-                    retentionTagOverrides: [
-                        {
-                            tagName: "Default",
-                            retentionDuration: {
-                                count: 30,
-                                durationType: "Days",
-                            },
-                        },
-                    ],
+// Create a Backup Policy
+if (vault) {
+    const backupPolicy = new recoveryservices.ProtectionPolicy("DailyPolicy", {
+        resourceGroupName: resourceGroup,
+        vaultName: vault.name,
+        properties: {
+                backupManagementType: "AzureIaasVM",
+                instantRPDetails: {},
+                schedulePolicy: {
+                    schedulePolicyType: "SimpleSchedulePolicy",
+                    scheduleRunFrequency: "Daily",
+                    scheduleRunTimes: ["2021-09-01T00:00:00Z"],
+                    scheduleWeeklyFrequency: 0,
                 },
-                trigger: {
-                    schedule: {
-                        recurrence: {
-                            interval: "Day",
-                            count: 1,
-                        },
-                        timeZone: "UTC",
-                        scheduleRunTimes: ["2022-01-01T00:00:00Z"],
+                tieringPolicy: {
+                    ArchivedRP: {
+                        tieringMode: "DoNotTier",
+                        duration: 0,
+                        durationType: "Invalid"
                     },
-                    objectType: "ScheduleBasedTriggerContext",
                 },
-            },
-        ],
-        datasourceTypes: ["AzureVm"],
-    },
-});
- */
+                instantRpRetentionRangeInDays:3,
+                timeZone: "UTC",
+                    protectedItemsCount: 0,
+                },
+            });
+        }
